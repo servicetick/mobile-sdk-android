de7993820b345863d7dcdded2e9b151b8a875726
lilHermit <developer@lilhermit.co.uk>
2018-11-21 16:15:12 +0000

    Make SurveyPageTransition parcelable

--- library/src/main/java/com/servicetick/android/library/entities/SurveyPageTransition.kt
+++ library/src/main/java/com/servicetick/android/library/entities/SurveyPageTransition.kt
@@ -1,12 +1,13 @@
 package com.servicetick.android.library.entities
 
+import android.os.Parcel
 import androidx.room.Entity
 import androidx.room.Ignore
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
 @Entity(tableName = "survey_page_transitions", primaryKeys = ["surveyId", "sourcePageId"])
-internal class SurveyPageTransition {
+internal class SurveyPageTransition() : KParcelable {
 
     var surveyId: Long = 0
 
@@ -27,7 +28,27 @@ internal class SurveyPageTransition {
     @Ignore
     var conditions: List<Any> = emptyList()
 
+    constructor(parcel: Parcel) : this() {
+        surveyId = parcel.readLong()
+        sourcePageId = parcel.readLong()
+        isCompletionPage = parcel.readBoolean()
+        targetPageId = parcel.readLong()
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeLong(surveyId)
+        parcel.writeLong(sourcePageId)
+        parcel.writeBoolean(isCompletionPage)
+        parcel.writeValue(targetPageId)
+    }
+
     override fun toString(): String {
         return "SurveyPageTransitions(surveyId=$surveyId, sourcePageId=$sourcePageId, isCompletionPage=$isCompletionPage, targetPageId=$targetPageId, conditions=$conditions)"
+    }
+
+    companion object {
+
+        @JvmField
+        val CREATOR = parcelableCreator(::SurveyPageTransition)
     }
 }
