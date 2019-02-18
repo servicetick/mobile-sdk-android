9d494248d73ff50944b8ee5e2c0aa771b59be9d4
lilHermit <developer@lilhermit.co.uk>
2019-02-18 10:51:33 +0000

    Changed SurveysViewModel so it can handle different lifecycles (Now works with shared viewmodel)     Now stores the survey so it can be reused

--- library/src/main/java/com/servicetick/android/library/viewmodel/SurveysViewModel.kt
+++ library/src/main/java/com/servicetick/android/library/viewmodel/SurveysViewModel.kt
@@ -1,6 +1,8 @@
 package com.servicetick.android.library.viewmodel
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
 import com.servicetick.android.library.entities.Survey
 import com.servicetick.android.library.entities.SurveyQuestion
@@ -8,6 +10,40 @@ import com.servicetick.android.library.repository.SurveyRepository
 
 internal class SurveysViewModel constructor(private val surveyRepository: SurveyRepository) : ViewModel() {
 
-    fun getSurvey(id: Long): LiveData<Survey?> = surveyRepository.getSurvey(id)
-    fun getQuestionsForPage(sourcePageId: Long): LiveData<List<SurveyQuestion>> = surveyRepository.getQuestionsForPage(sourcePageId)
+    private lateinit var survey: Survey
+
+    fun getSurvey(id: Long): LiveData<Survey?> {
+        val liveData = MutableLiveData<Survey?>()
+        if (!::survey.isInitialized || survey.id != id) {
+            val tempLiveData = surveyRepository.getSurvey(id)
+            tempLiveData.observeForever(object : Observer<Survey?> {
+                override fun onChanged(t: Survey?) {
+                    t?.let {
+                        survey = it
+                    }
+                    liveData.postValue(t)
+                    tempLiveData.removeObserver(this)
+                }
+            })
+        } else {
+            liveData.postValue(survey)
+        }
+        return liveData
+    }
+
+    fun getQuestionsForPage(sourcePageId: Long): LiveData<List<SurveyQuestion>> {
+
+        val liveData = MutableLiveData<List<SurveyQuestion>>()
+        if (::survey.isInitialized) {
+            val questions = survey.questions.filter {
+                it.pageId == sourcePageId
+            }.sortedBy {
+                it.questionOrder
+            }
+
+            liveData.postValue(questions)
+        }
+        return liveData
+
+    }
 }
