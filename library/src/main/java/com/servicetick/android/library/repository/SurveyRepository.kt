0599f086f776c4672355efd88f3e231dad60efaa
lilHermit <developer@lilhermit.co.uk>
2019-02-20 09:36:23 +0000

    Added save SurveyResponse method

--- library/src/main/java/com/servicetick/android/library/repository/SurveyRepository.kt
+++ library/src/main/java/com/servicetick/android/library/repository/SurveyRepository.kt
@@ -5,10 +5,17 @@ import com.servicetick.android.library.AppExecutors
 import com.servicetick.android.library.db.ServiceTickDao
 import com.servicetick.android.library.entities.Survey
 import com.servicetick.android.library.entities.SurveyQuestion
+import com.servicetick.android.library.entities.SurveyResponse
 
 
 internal class SurveyRepository constructor(private val serviceTickDao: ServiceTickDao, private val appExecutors: AppExecutors) {
 
     fun getSurvey(id: Long): LiveData<Survey?> = serviceTickDao.getSurveyAsLiveData(id)
     fun getQuestionsForPage(pageId: Long): LiveData<List<SurveyQuestion>> = serviceTickDao.getQuestionsForPageAsLiveData(pageId)
+
+    fun saveResponse(surveyResponse: SurveyResponse) {
+        appExecutors.diskIO().execute {
+            serviceTickDao.insert(surveyResponse)
+        }
+    }
 }
