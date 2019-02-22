3c1c82360ac4d2ae5b839b2e26cd9cf095877e1d
lilHermit <developer@lilhermit.co.uk>
2019-02-22 14:06:37 +0000

    Added in slider render to RadioQuestionView

--- library/src/main/java/com/servicetick/android/library/view/questions/RadioQuestionView.kt
+++ library/src/main/java/com/servicetick/android/library/view/questions/RadioQuestionView.kt
@@ -14,11 +14,18 @@ import androidx.core.content.getSystemService
 import androidx.core.view.forEach
 import com.servicetick.android.library.R
 import com.servicetick.android.library.entities.SurveyQuestionOption
+import com.warkiz.widget.IndicatorSeekBar
+import com.warkiz.widget.OnSeekChangeListener
+import com.warkiz.widget.SeekParams
+
 
 internal class RadioQuestionView @TargetApi(Build.VERSION_CODES.LOLLIPOP)
 constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : QuestionView(context, attrs, defStyleAttr) {
 
     private var radioGroup: RadioGroup? = null
+    private var indicatorSeekBar: IndicatorSeekBar? = null
+    private var renderSlider: Boolean = false
+    private var sliderValue: Int? = null
 
     init {
         postLayout(context.getSystemService<LayoutInflater>()?.inflate(R.layout.view_question_radio, this, true))
@@ -31,6 +38,7 @@ constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : Questio
         super.postLayout(view)
         view?.run {
             radioGroup = findViewById(R.id.radioGroup)
+            indicatorSeekBar = findViewById(R.id.indicatorSeekBar)
         }
     }
 
@@ -38,11 +46,50 @@ constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : Questio
     override fun updateView() {
         super.updateView()
 
-        radioGroup?.run {
+        question?.let { surveyQuestion ->
 
-            question?.let { surveyQuestion ->
-                surveyQuestion.options?.forEach {
-                    addView(radioButton(it), RadioGroup.LayoutParams(0, RadioGroup.LayoutParams.WRAP_CONTENT, 1f))
+            renderSlider = surveyQuestion.options?.none {
+                !it.descriptor.isNullOrBlank()
+            } == false
+
+            if (renderSlider) {
+
+                indicatorSeekBar?.run {
+                    visibility = View.VISIBLE
+
+                    max = surveyQuestion.options?.size?.minus(1)?.toFloat() ?: 0f
+                    min = 0f
+
+                    customTickTexts(arrayOf(
+                            surveyQuestion.options?.first()?.descriptor,
+                            surveyQuestion.options?.last()?.descriptor))
+
+                    if (question?.answer?.answer != null) {
+                        val progress = surveyQuestion.options?.indexOfFirst {
+                            it.id == getAnswerId()
+                        }?.toFloat() ?: 0f
+
+                        setProgress(progress)
+                        sliderValue = progress.toInt()
+                    }
+
+                    onSeekChangeListener = object : OnSeekChangeListener {
+                        override fun onStartTrackingTouch(seekBar: IndicatorSeekBar?) {}
+
+                        override fun onStopTrackingTouch(seekBar: IndicatorSeekBar?) {}
+
+                        override fun onSeeking(seekParams: SeekParams?) {
+                            sliderValue = seekParams?.progress
+                        }
+                    }
+                }
+            } else {
+                radioGroup?.run {
+                    visibility = View.VISIBLE
+
+                    surveyQuestion.options?.forEach {
+                        addView(radioButton(it), RadioGroup.LayoutParams(RadioGroup.LayoutParams.MATCH_PARENT, RadioGroup.LayoutParams.WRAP_CONTENT))
+                    }
                 }
             }
         }
@@ -64,7 +111,11 @@ constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : Questio
 
     override fun isValid(): Boolean {
 
-        val valid = super.isValid() || minRequiredAnswers() == 0 || (maxRequiredAnswers() != 0 && radioGroup?.checkedRadioButtonId != -1)
+        val valid = super.isValid() || minRequiredAnswers() == 0 || if (renderSlider) {
+            (maxRequiredAnswers() != 0 && sliderValue != null)
+        } else {
+            (maxRequiredAnswers() != 0 && radioGroup?.checkedRadioButtonId != -1)
+        }
 
         if (!valid) {
             // TODO Error handling
@@ -76,7 +127,13 @@ constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : Questio
 
     override fun syncAnswer() {
         if (isAnswerSyncable()) {
-            question?.answer?.answer = getCheckedIds().joinToString()
+            if (renderSlider) {
+                sliderValue?.let { value ->
+                    question?.answer?.answer = question?.options?.get(value)?.id.toString()
+                }
+            } else {
+                question?.answer?.answer = getCheckedIds().joinToString()
+            }
         }
     }
 }
